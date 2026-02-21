967275a25e841ecae5eca0558a9b777609c85aa8
ravindu <ravindu.peiris11@gmail.com>
2026-02-21 21:00:44 +0530

    Promises

---
+++ Promises.js
@@ -0,0 +1,10 @@
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) resolve('Success!');
+  else reject('Failure!');
+});
+
+myPromise
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
